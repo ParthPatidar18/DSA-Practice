e0aef963df7cf56a1d51cfb8ea838f29381609dd
Parth Patidar <127079561+ParthPatidar18@users.noreply.github.com>
2025-10-21 16:42:55 +0530

    Implement minimum cost connection between cities

---
+++ Graph/FindMinCostBtwCities.java
@@ -0,0 +1,48 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class FindMinCostBtwCities {
+    static class Edge implements Comparable<Edge>{
+        int dest,cost;
+        public Edge(int d, int c){
+            this.dest=d;
+            this.cost=c;
+        }
+        @Override
+        public int compareTo(Edge e2){
+            return this.cost - e2.cost;
+        }
+    }
+    public static int connectCities(int cities[][]){
+        PriorityQueue<Edge> p=new PriorityQueue<>();
+        boolean vis[]=new boolean[cities.length];
+        p.add(new Edge(0,0));
+        int finalCost=0;
+        while(!p.isEmpty()){
+            Edge curr = p.remove();
+            if(!vis[curr.dest]){
+                vis[curr.dest]=true;
+                finalCost +=curr.cost;
+                
+                for(int i=0;i<cities[curr.dest].length;i++){
+                    if(cities[curr.dest][i] != 0){
+                        p.add(new Edge(i,cities[curr.dest][i]));
+                    }
+                }
+            }
+        }
+        return finalCost;
+        
+    }
+    public static void main(String[] args) {
+       int cities[][]={
+           {0,1,2,3,4},
+           {1,0,5,0,7},
+           {2,5,0,6,0},
+           {3,0,6,0,0},
+           {4,7,0,0,0}   
+           };
+           
+           System.out.print(connectCities(cities));
+    }
+}
